454f92cc802e786299b0514520f01fb23c7d6972
ichacahyawulan <45441252+ichacahyawulan@users.noreply.github.com>
2020-12-22 09:16:38 +0700

    Update News.vue

--- web-travel-vue/src/components/News.vue
+++ web-travel-vue/src/components/News.vue
@@ -1,39 +1,12 @@
 <template>
-  <div v-if="currentNews" class="edit-form">
-    <h4>News</h4>
-    <form>
-      <div class="form-group">
-        <label for="title">Title</label>
-        <input
-          type="text"
-          class="form-control"
-          id="title"
-          v-model="currentNews.title"
-        />
-      </div>
-      <div class="form-group">
-        <label for="content">Content</label>
-        <input
-          type="text"
-          class="form-control"
-          id="content"
-          v-model="currentNews.content"
-        />
-      </div>
-    </form>
-
-    <button class="badge badge-danger mr-2" @click="deleteNews">Delete</button>
-
-    <button type="submit" class="badge badge-success" @click="updateNews">
-      Update
-    </button>
-    <p>{{ message }}</p>
-  </div>
-
-  <div v-else>
-    <br />
-    <p>Please click on a Tutorial...</p>
-  </div>
+  <body>
+    <div class="event-card">
+      <h1>{{ currentNews.title }}</h1>
+      <h3>{{ currentNews.author }}</h3>
+      <h6>Published: {{ currentNews.publish }}</h6>
+      <p>Content: {{ currentNews.content }}</p>
+    </div>
+  </body>
 </template>
 
 <script>
@@ -83,12 +56,56 @@ export default {
   },
   mounted() {
     this.message = "";
-    this.getTutorial(this.$route.params.id);
+    this.getNews(this.$route.params.id);
   },
 };
 </script>
 
-<style>
+<style scoped>
+.container li {
+  list-style: none;
+  margin: 2em 0;
+}
+
+.event-card {
+  overflow: hidden;
+  width: 100%;
+  margin: 120px auto auto;
+  border-radius: 0.3em;
+}
+
+.event-card img {
+  width: 300px;
+  height: 200px;
+  object-fit: cover;
+}
+
+.event-card .judul {
+  font-size: 2em;
+  font-weight: 400;
+  padding-top: 0.2em;
+  padding-left: 0.5em;
+}
+.event-card .author {
+  font-size: 1.2em;
+  font-weight: 400;
+  padding-left: 1em;
+  padding-bottom: 1em;
+}
+.h1 {
+  text-align: center;
+  padding-top: 30px;
+  padding-bottom: 30px;
+}
+.btn {
+  height: 38px;
+  text-align: center;
+}
+
+.search {
+  margin: 120px auto auto;
+  width: 65%;
+}
 .edit-form {
   max-width: 300px;
   margin: auto;
