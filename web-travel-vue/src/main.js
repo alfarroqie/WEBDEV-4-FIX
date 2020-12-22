837f12bd654c3f8de9d05cae4b1811f2bc4e996b
ZaniRiyan <45956030+ZaniRiyan@users.noreply.github.com>
2020-12-23 00:58:23 +0700

    Add files via upload

--- web-travel-vue/src/main.js
+++ web-travel-vue/src/main.js
@@ -1,10 +1,14 @@
 import Vue from 'vue'
-import App from './components/Cms.vue'
+// import App from './components/Cms.vue'
+// import App from './App.vue'
+import App from './components/Login.vue'
 import router from './router'
+import vuetify from './plugins/vuetify';
 
 Vue.config.productionTip = false
 
 new Vue({
   router,
-  render: h => h(App),
+  vuetify,
+  render: h => h(App)
 }).$mount('#app')
