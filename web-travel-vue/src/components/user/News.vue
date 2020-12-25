afe0998ac7a27b13273a34ba0189a7091dccfdf7
ichacahyawulan <45441252+ichacahyawulan@users.noreply.github.com>
2020-12-25 23:53:47 +0700

    Update and rename web-travel-vue/src/components/News.vue to web-travel-vue/src/components/user/News.vue

--- web-travel-vue/src/components/user/News.vue
+++ web-travel-vue/src/components/user/News.vue
@@ -22,7 +22,7 @@
 </template>
 
 <script>
-import NewsDataService from "../services/NewsDataService";
+import NewsDataService from "../../services/NewsDataService";
 export default {
   name: "news",
   data() {
